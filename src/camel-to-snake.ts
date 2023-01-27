a35df7f596ac3152fc927c05daa25ed62e229a91
Kenty Akoarum <kamome-black@akoarum.net>
2023-01-27 13:17:10 +0900

    if argument is not a string, it is not processed and returned as is

--- src/camel-to-snake.ts
+++ src/camel-to-snake.ts
@@ -1,4 +1,5 @@
 export const camelToSnake = (words: string) => {
+  if (typeof words !== 'string') return words
   const upperToSnake = (word: string) => `_${word.charAt(0).toLowerCase()}`
   return words.replace(/[A-Z]/g, upperToSnake)
 }
--- src/snake-to-camel.ts
+++ src/snake-to-camel.ts
@@ -1,4 +1,5 @@
 export const snakeToCamel = (words: string) => {
+  if (typeof words !== 'string') return words
   const targetWord = words.replace(/^_(.*?)$/, '$1')
   const snakeToUpper = (word: string) => word.charAt(1).toUpperCase()
   return `${words.charAt(0) === '_' ? '_' : ''}${targetWord.replace(/_./g, snakeToUpper)}`
